cd07846ec49a07d6c9310310ba28475c2a0a246b
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-02-20 18:10:58 -0400

    Finalizei o carrossel

---
+++ script.js
@@ -0,0 +1,32 @@
+const carrossel = document.querySelector(".appleTV");
+const track = document.querySelector(".track");
+const slides = Array.from(track.children);
+
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const dotsContainer = document.querySelector(".dots");
+
+let index = 0;
+
+slides.forEach((_, i) => {
+  const dots = document.createElement("p");
+  dots.className = "dot";
+  dots.addEventListener("click", () => goto(i));
+  dotsContainer.appendChild(dots);
+});
+
+const dots = Array.from(dotsContainer.children);
+
+const update = () => {
+  track.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach((d) => d.classList.remove("active"));
+  dots[index].classList.add("active");
+};
+
+const goto = (i) => {
+  index = (i + slides.length) % slides.length; // loop infinito
+  update();
+};
+
+prevBtn.addEventListener("click", () => goto(index - 1));
+nextBtn.addEventListener("click", () => goto(index + 1));
